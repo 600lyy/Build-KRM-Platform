9c8cbc79d9950633a3347f673cdcc98c581250a9
Andy Lu <luyul@google.com>
2022-11-23 21:55:10 +0100

    add two new namespaces

--- install.sh
+++ install.sh
@@ -67,8 +67,8 @@ sed -i "s/GITHUB_USERNAME/${GITHUB_USERNAME}/g" apply-spec.yaml
 
 # Install Config Sync on clusters
 
-#register_cluster "admin" "europe-north1-a"
-#install_config_sync "admin" "europe-north1-a"
+register_cluster "admin" "europe-north1-a"
+install_config_sync "admin" "europe-north1-a"
 
 register_cluster "prod-eu" "europe-north1-a"
 install_config_sync "prod-eu" "europe-north1-a"
